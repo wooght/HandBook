eb075ab938324b58c518a20c15ba7f925b823c07
wooght <wooght@126.com>
2018-03-30 01:56:54 +0800

    add BernoulliNB

--- python/machine-learn/NBC.py
+++ python/machine-learn/NBC.py
@@ -14,7 +14,7 @@ from sklearn import metrics
 def m_nb(x, y):
     from sklearn.naive_bayes import MultinomialNB
     model = MultinomialNB(alpha=0.9)  # alpha默认为1 根据拟合效果可适当调整
-    model.fit(x, y)  # 数据拟合
+    model.fit(x, y)  # 数据拟合,训练 x:样本/类别,y:为样本/类别标签
     return model
 
 
@@ -34,7 +34,7 @@ def b_nb(x, y):
     return model
 
 
-x1 = np.array([[1, 1, 3, 4, 5], [5, 4, 3, 2, 1]])  # 样本
+x1 = np.array([[1, 2, 3, 4, 5], [5, 4, 3, 2, 1]])  # 样本
 y1 = np.array([0, 1])  # 样本标签
 t1 = np.random.randint(6, size=(1, 5))  # 测试数据
 yb = m_nb(x1, y1)
@@ -61,8 +61,46 @@ attitude_model = m_nb([pos_words, neg_words], np.array([0, 1]))
 test_words = pd.Series({'a': 4, 'b': 0, 'c': 0, 'd': 3, 'e': 0})
 result_predict = attitude_model.predict([test_words])
 result_proba = attitude_model.predict_proba([test_words])
-print(result_proba)
-print('分析结果,pos:', '%.5f' % result_proba[0][0], ' neg:', '%.5f' % result_proba[0][1], result_predict)
+print('文本分类,pos:', '%.5f' % result_proba[0][0], ' neg:', '%.5f' % result_proba[0][1], result_predict)
+
+test_words = pd.Series({'a': 159, 'b': 120, 'c': 70, 'd': 123, 'e': 150})
+result_predict = attitude_model.predict([test_words])
+result_proba = attitude_model.predict_proba([test_words])
+print('文本分类,pos:', '%.5f' % result_proba[0][0], ' neg:', '%.5f' % result_proba[0][1], result_predict)
+
+
+# 性别分类实例
+# 男 H:160-190, T:110-210, X:39-44
+# 女 H:150-175, T:80-160, X:36-41
+man_h = np.random.randint(160, 190, size=(100, 1))
+man_t = np.random.randint(100, 210, size=(100, 1))
+man_x = np.random.randint(39, 44, size=(100, 1))
+man = np.column_stack([man_h, man_t, man_x])
+# for _ in 的应用,循环生成指定格式数组的方式
+wman = [[np.random.randint(150, 175), np.random.randint(80, 160), np.random.randint(36, 41)] for _ in range(100)]
+person_train = np.row_stack([man, wman])
+print(person_train.shape)
+y_train = np.random.randint(1, size=(200))
+y_train[-100:] = 1
+print(y_train.shape)
+person_model = m_nb(person_train, y_train)
+person_result = person_model.predict_proba(np.array([[171, 140, 39]]))
+print('性别概率:', person_result)
+
+
+
+# 得病概率
+x_z = np.random.randint(1, size=(9900, 2))  # 正常人
+x_z[-99:] = [0, 1]  # 误诊阳性
+x_h = np.random.randint(1, 2, size=(100, 2))  # 患病人
+x_h[-1] = [1, 0]  # 误诊阴性
+x_train = np.row_stack([x_z, x_h])
+y = np.random.randint(1, size=(10000))
+y[-100:] = 1  # 患病标识为1
+print(y.shape)
+hb_model = b_nb(x_train, y)
+hb_result = hb_model.predict_proba(np.array([[0, 1]]))  # 对于0,1特征的样本,采用 BernoulliNB才准确
+print('得病概率:', hb_result)
 
 
 X = np.array([
@@ -71,10 +109,12 @@ X = np.array([
 ])
 X = X.T  # 转换
 y = np.array([-1, -1, 1, 1, -1, -1, -1, 1, 1, -1, 1, 1, 1, 1, -1])
+print(X.shape)
+print(y.shape)
 print(np.column_stack((X, y)))
 from sklearn.naive_bayes import MultinomialNB
 
 nb = MultinomialNB(alpha=1, fit_prior=True)
 nb.fit(X, y)
-print(nb.predict(np.array([[3, 5]])))
-print(nb.predict_proba(np.array([[3, 5], [2, 5]])))
+print(nb.predict(np.array([[1, 6]])))
+print(nb.predict_proba(np.array([[1, 6], [2, 5]])))
--- python/machine-learn/linear.py
+++ python/machine-learn/linear.py
@@ -10,7 +10,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-
 data = pd.read_csv('./data/Folds5x2_pp.csv')
 print(data.head())  # 输出前五条
 print(data.shape)  # 查看数据维度
@@ -22,7 +21,8 @@ print(y.head())
 
 # 划分训练集和测试集 train为训练街,test为测试集
 # X_train 为训练自变量,y_train 为自变量对应的因变量
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)  # random_state 随机数种子,每次都填写1,那么每次的随机数相同,及数组排序的顺序
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    random_state=1)  # random_state 随机数种子,每次都填写1,那么每次的随机数相同,及数组排序的顺序
 print(X_train.shape)
 print(X_test.shape)
 print(y_train.shape)
@@ -48,8 +48,9 @@ print(y_pred)
 # 模型评价
 # 两种方式:MSE 均方误差，RMSE均方根误差
 from sklearn import metrics
+
 print('MSE', metrics.mean_squared_error(y_test, y_pred))  # 对模型进行评价,不同方法得到的不同系数,选择MSE最小的时候对应的方法或者系数最佳
-print('RMSE',np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+print('RMSE', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
 # 少选择一个特征 看系数是否更佳
 X = data[['AT', 'V', 'RH']]  # 原来为: ['AT', 'V', 'AP', 'RH']
@@ -57,3 +58,15 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 linreg = one_linear(X_train, y_train)
 y_pred = linreg.predict(X_test)
 print('New MSE:', metrics.mean_squared_error(y_test, y_pred))  # 如果小于之前的MSE  证明这里少一个特征得到的系数更佳,模型就更佳
+
+
+# 行情数据关系测试
+stock_data = pd.read_csv('./data/data.csv')
+print(stock_data.head(100))
+stock_train = stock_data[['shou_change', 'dd_change', 'zd_money']]
+last_data = stock_train[stock_train['shou_change'] != 0.]
+print(last_data.head(100))
+X_t = last_data[['shou_change', 'dd_change']]
+y_t = last_data['zd_money']
+stock_linreg = one_linear(X_t, y_t)
+print(stock_linreg.intercept_, stock_linreg.coef_)  # 0.0455617718101 [ 2.79945893  1.49123189]
